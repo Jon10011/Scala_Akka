366bfe32875485705981f09df5e1ce959e755293
Jon <dong_song1@163.com>
2019-07-07 19:40:09 +0800

    Scala Akka入门

--- src/main/scala/com/songsong/actors/BActor.scala
+++ src/main/scala/com/songsong/actors/BActor.scala
@@ -6,6 +6,7 @@ class BActor extends Actor{
   override def receive: Receive = {
     case "我打" => {
       println("BActor(乔峰) 挺猛 看我降龙十八掌")
+      //sender()获取到发送Actor的代理/引用对象 ref
       sender() ! "我打"
     }
   }
